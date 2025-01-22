5187ac9b07db4f75b823c9a3d630b561b032a888
fmcastells <fcastells@gmail.com>
2025-01-21 22:12:13 -0500

    Add missing scanner close

--- src/main/java/ca/uqam/example/Application.java
+++ src/main/java/ca/uqam/example/Application.java
@@ -33,6 +33,7 @@ public class Application {
             String continueChoice = scanner.nextLine();
             if ("no".equalsIgnoreCase(continueChoice)) {
                 shouldContinue = false;
+                scanner.close();
             }
         }
     }
